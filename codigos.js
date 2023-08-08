356976867d853f3f549c03e3c0f25a0ec02cbf72
ositapooh <salinascatalina156@gmail.com>
2023-08-08 12:11:28 -0300

    subir nuevo arcihvo

---
+++ codigos.js
@@ -0,0 +1,94 @@
+saludo = " !Hola pedro! " ;
+pregunta = " ¿como estas? " ;
+frase = saludo + pregunta ;
+//document.write(frase) (+ suma una cosa a las otras)
+
+ 
+numero1 = 10;
+numero2 = 5;
+resultado = numero1 + numero2
+//alert(resultado)
+
+numero1 = 10;
+numero2 = 5
+resultado = numero1/numero2;
+//alert(resultado);  (/para dividir)
+
+x=10
+y=5;
+x = x + y;
+//alert("Resultado:  " + x);  (=asigna el valor del elemento, ya sea y o p)
+
+nombre = " Osita "; 
+frase2 =   ` soy ${nombre}estoy estudiando` ;
+//document.write(frase2);
+
+
+let numero5 = 45;
+let numero8 = 83;
+//document.write(numero5 ==numero8)IGUAL
+
+
+let numero79 = 75;
+let numero98 = 68;
+//document.write(numero79!==numero98)DESIGUAL
+
+let numero65 = 744;
+let numero92 = 655;
+//document.write(numero65*numero92)MULTIPLICACION
+
+let texto = "texto1";
+let texto2 = "texto2";
+//document.write(texto != texto2)
+
+ let numero3 = 45;
+ let numero4 = 45;
+  
+//document.write(numero3 <= numero4)MENOS QUE(IZQUIERDA)
+
+let numero51 = 34;
+let numero84 = 54;
+//document.write(numero51>numero84)MAYOR QUE DERECHA
+
+let numero93 = 19;
+let numero73 = 19;
+//document.write(numero93<=numero73)MENOR O IGUAL
+
+
+num2 = 72;
+num3 = 28;
+afirmacion1 = num2 > num3;
+afirmacion2 = num3 != num2;
+//document.write(afirmacion1 && afirmacion2)
+
+
+num55 = 77;
+num88 = 88;
+afirmacion7 = num55 > num88 ;
+afirmacion8 = num88 != num55 ;
+//document.write(afirmacion7 && afirmacion8) 
+
+
+
+num4 = 35;
+num5 = 76;
+afirmacion1 = num4 > num5 ; 
+afirmacion2 = num5 < num4; 
+//document.write(afirmacion1 || afirmacion2)
+
+//palabra1Palabra2DecimeAlgoPorfavor
+
+let nombrr = [" de catalina","stefania","salinas, para"];
+//document.write(nombrr [2]);
+
+
+let ganso = ["  dario","mosquera te amoo"];
+//document.write(ganso[1]);
+
+let carta = {
+    De:"tunovia",
+    Para:"minovio",
+    dia:"dos"
+ }
+
+ document.write(carta)
